63ab57fe2b34785756ef745c160374ac22341f01
Twijg <twijg54@protonmail.com>
2021-07-09 13:41:52 +0200

    Gewerkt aan RE

--- Automata/Bert/RE.cpp
+++ Automata/Bert/RE.cpp
@@ -1,3 +1,58 @@
 
 #include "RE.h"
 
+void RE::print() {
+    std::cout << expressie;
+}
+
+void RE::addToAlphabet(std::string& in) {
+
+}
+
+REnode::REnode(std::string& name) {
+    naam = name;
+}
+
+RE::RE(std::string inp) {
+    char c;
+    for (int i = 0; i <= inp.size(); i++) {
+        c= inp[i];
+        if (c == '(') {
+            int haakjescounter = 1;
+            std::string subexp;
+            int j = 1;
+            while (haakjescounter != 0) {
+                if (inp[i+j] == '(') {
+                    haakjescounter ++;
+                    subexp += inp[i+j];
+                } else if (inp[i+j] == ')') {
+                    haakjescounter --;
+                    if (haakjescounter != 0) {
+                        subexp += inp[i+j];
+                    }
+                }
+            }
+
+
+        } else if (c == '+') {
+
+        } else if (c == '*') {
+
+        } else { // gewoon karakter
+            std::string naam;
+
+
+            while (inp[i] != '(' and inp[i] != ')' and inp[i] != '+' and inp[i] != '*') {
+                naam += c;
+                i++;
+            }
+            addToAlphabet(naam);
+            i--;
+            REnode nieuw(naam);
+
+            nodes.push_back(nieuw);
+        }
+    }
+}
+
+
--- Automata/Bert/RE.h
+++ Automata/Bert/RE.h
@@ -1,19 +1,36 @@
 #include <string>
 
 
+
 #ifndef CLION_RE_H
 #define CLION_RE_H
 
+#include <vector>
+#include "ENFA.h"
 
+class REnode {
+public:
+    std::string naam;
+    REnode(std::string& name);
+
+};
 
 class RE {
 public:
+    std::string eps;
+    std::vector<REnode> startnodes;
+    std::vector<REnode> eindnodes;
+    std::vector<std::string> alfabet;
     std::string expressie;
-    char eps;
-    RE(std::string a, std::string b) {
-        expressie = a;
-        eps = b[0];
-    }
+    std::vector<REnode> nodes;
+
+    void addToAlphabet(std::string& in);
+
+    RE(std::string);
+    void print();
+    ENFA toENFA();
+    bool accepts();
+
 
 };
 
