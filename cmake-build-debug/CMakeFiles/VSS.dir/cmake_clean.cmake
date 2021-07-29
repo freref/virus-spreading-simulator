2e5da59e4cd73dfbb2d61c287f894f660643b437
frehml <frederic.hamelink@hotmail.com>
2021-07-29 17:32:41 +0200

    small changes

---
+++ cmake-build-debug/CMakeFiles/VSS.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VSS.dir/Automata.cpp.obj"
+  "CMakeFiles/VSS.dir/Automata/Frederic/DFA.cpp.obj"
+  "CMakeFiles/VSS.dir/Automata/Frederic/ENFA.cpp.obj"
+  "CMakeFiles/VSS.dir/Automata/Frederic/NFA.cpp.obj"
+  "CMakeFiles/VSS.dir/Automata/Frederic/RE.cpp.obj"
+  "CMakeFiles/VSS.dir/Correctness.cpp.obj"
+  "CMakeFiles/VSS.dir/Mens.cpp.obj"
+  "CMakeFiles/VSS.dir/Simulatie.cpp.obj"
+  "CMakeFiles/VSS.dir/Statistiek.cpp.obj"
+  "CMakeFiles/VSS.dir/Virus.cpp.obj"
+  "CMakeFiles/VSS.dir/World.cpp.obj"
+  "CMakeFiles/VSS.dir/main.cpp.obj"
+  "VSS.exe"
+  "VSS.exe.manifest"
+  "VSS.pdb"
+  "libVSS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VSS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
