d2a67317f7a1f2d92ecc98d7a9e399e281ae9678
frehml <frederic.hamelink@hotmail.com>
2024-10-22 14:36:50 +0200

    remove idea and debug

--- cmake-build-debug/CMakeFiles/VSS_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/VSS_autogen"
-  "VSS_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/VSS_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
